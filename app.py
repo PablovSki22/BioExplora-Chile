b788b55025338a751a59206e920cc969b785ee1d
PablovSki22 <pabloibarravallejos@gmail.com>
2026-07-31 08:08:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ st.set_page_config(
 )
 st.markdown("""
 <link rel="manifest" href="manifest.json">
+<meta name="theme-color" content="#0b132b">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="apple-mobile-web-app-title" content="BioExplora Chile">
+<link rel="apple-touch-icon" href="favicon_512.png">
 <script>
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
