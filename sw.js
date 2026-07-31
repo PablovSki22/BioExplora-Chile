d0e3fe5808e6ab8ce3752997a0fc9d87ab3f0f7c
PablovSki22 <pabloibarravallejos@gmail.com>
2026-07-31 06:26:59 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'bioexplora-cache-v1';
+const urlsToCache = [
+  '/',
+  'https://raw.githubusercontent.com/PablovSki22/BioExplora-Chile/main/favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
